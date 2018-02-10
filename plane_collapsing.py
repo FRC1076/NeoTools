110257831ffb58673817ead3461ec5888814dc55
Matthew Jones <mcolinj@mac.com>
2018-02-10 11:49:12 -0500

    Add additional comments to collapse_planes function (and some hints).

--- plane_collapsing.py
+++ plane_collapsing.py
@@ -9,7 +9,7 @@ def test_fake_combiner():
     assert(combiner([1,2,3,4]) == 1234)
 
 def combiner(values):
-  return values[0]*1000 + values[1]*100 + values[2]*10 + values[3]
+    return values[0]*1000 + values[1]*100 + values[2]*10 + values[3]
 
 
 #  Some .png image formats provide image data as an array of color values
@@ -39,13 +39,6 @@ def combiner(values):
 #  So, we could represent the image data for 4000 pixels with 500 bytes.
 #  One bit for each pixel.   And then a few more bytes for the palette.
 #
-#  For this task, you need to write the collapse function that collapses
-#  4 planes into a single value for the entire list.
-#
-#  You might want to try it using iteration (using for loops with index+=4,
-#  using recursion, and even using functional style to see what works best,
-#  and to sharpen your python skills.
-#
 #  Now here are the tests to work on your collapse_planes function.  It should
 #  combine 4 numbers into a single value.    So, for example, a list of
 #  40 values should be combined into a list of 10 values.
@@ -64,5 +57,20 @@ def test_many_collapse():
     assert(collapse_planes([0,0,0,0,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,2,3,4,5,1,2,3,4]) == [0,1234,5678,1234,5678,2345,1234])
 
 def collapse_planes(planes):
+    """
+    Given a list of color planes, reduce the list by combining factor elements
+    using the a combiner function  (see above).   We will change that fn later and use
+    methods in the NeoPalette class the do both collapse and combine.   But for now just
+    figure out how to do this elegantly.
+    
+    You might want to try it using iteration (using for loops with index+=4,
+    using recursion, and even using functional style to see what works best,
+    and to sharpen your python skills.   Ask for hints if you want some.
+
+    Here are a few:
+          You can slice four elements from an array A using:   A[0:4]
+          You can refer to the remainder of the array A as:    A[4:]
+          This could help in writing a recursive version of this function.
+    """
     return 0
 
